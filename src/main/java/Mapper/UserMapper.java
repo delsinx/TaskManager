e7aa448d9420c683e8c7eb7b70f6aa7ae5a5759a
Delson Antônio Andrade Filho <delson_filho@protonmail.com>
2025-12-01 15:01:09 -0300

    Added UserMapper

--- src/main/java/Mapper/UserMapper.java
+++ src/main/java/Mapper/UserMapper.java
@@ -1,4 +1,30 @@
 package Mapper;
 
+
+import DTO.UserDTO;
+import Model.UserModel;
+
 public class UserMapper {
+    public UserModel map(UserDTO userDTO) {
+        UserModel userModel = new UserModel();
+
+        UserModel.setId(UserDTO.getId());
+        UserModel.setName(UserDTO.getName());
+        UserModel.setEmail(UserDTO.getEmail());
+        UserModel.setPassword(UserDTO.getPassword());
+        UserModel.setCreatedAt(UserDTO.getCreatedAt());
+
+        return userModel;
+    }
+
+    public UserDTO map(UserModel userModel) {
+        UserDTO userDTO = new UserDTO();
+        UserModel.setId(UserDTO.getId());
+        UserModel.setName(UserDTO.getName());
+        UserModel.setEmail(UserDTO.getEmail());
+        UserModel.setPassword(UserDTO.getPassword());
+        UserModel.setCreatedAt(UserDTO.getCreatedAt());
+        return userDTO;
+    }
+
 }
